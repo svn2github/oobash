ed8cfd202e759736ab5202d9ab3d8a79163ba38b
andreasfrank <andreasfrank@d2084226-57a8-46eb-bcc5-3bd0c7bada0d>
2011-12-12 10:15:47 +0000

    version switch
    git-svn-id: http://svn.code.sf.net/p/oobash/code@64 d2084226-57a8-46eb-bcc5-3bd0c7bada0d

---
+++ 0.39/docs/examples/decorator.sh
@@ -0,0 +1,79 @@
+source ../../oobash-0.39.source 1>/dev/null 2>&1
+if (( $? != 0 ))
+then
+   echo "Sorry, but could not find oobash"
+   exit 0
+fi
+
+# START -- My own addon decorator
+@false()
+{
+   if (($# != 0))
+   then
+      System.err.println "I am false and will return 1"
+      return 1 
+   fi
+   return 0
+}
+# END -- My own addon decorator
+
+@deprecated
+function printer1()
+{
+   __decoratorCheck "$FUNCNAME" "$@"
+   echo "i print printer1"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+@calling
+function printer2() {
+   __decoratorCheck "$FUNCNAME" "$@"
+   echo "i print printer2"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+@calling
+@timestamp
+function printer3() {
+   __decoratorCheck "$FUNCNAME" "$@"
+   echo "i print printer3"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+function printer4() {
+   __decoratorCheck "$FUNCNAME" "$@"
+   echo "i print printer4"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+
+@calling
+@timestamp
+@false
+@deprecated
+function printer5() {
+   __decoratorCheck "$FUNCNAME" "$@"
+   echo My return value is $?, because of @false, but i could be a validator too!
+   echo "i print printer5"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+########
+# MAIN #
+########
+printer1
+printer2
+printer3 Arg1 Arg2
+printer4
+printer5 
+
