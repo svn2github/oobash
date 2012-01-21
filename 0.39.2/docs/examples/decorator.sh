20f33d13779269fe92fef1c9bfec3483624ca576
andreasfrank <andreasfrank@d2084226-57a8-46eb-bcc5-3bd0c7bada0d>
2012-01-21 19:21:47 +0000

    start 0.39.2
    git-svn-id: http://svn.code.sf.net/p/oobash/code@162 d2084226-57a8-46eb-bcc5-3bd0c7bada0d

---
+++ 0.39.2/docs/examples/decorator.sh
@@ -0,0 +1,88 @@
+source ../../oobash*.source 1>/dev/null 2>&1
+if (( $? != 0 )); then
+   echo "Sorry, but could not find oobash"
+   exit 0
+fi
+
+# START -- My own addon decorator
+# Do not use framework commands (e.g. System.out.println) in a decorator !
+@false() {
+   if (($# != 0)); then
+      echo  "I am false and will return 1" >&2
+      return 1 
+   fi
+   return 0
+}
+# END -- My own addon decorator
+
+@calling
+@timestamp
+printer1() {
+   __decoratorCheck  "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   echo "i print printer1"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+@calling
+printer2() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   echo "i print printer2"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+@deprecated
+@calling
+@timestamp
+printer3() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   echo "i print printer3"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+printer4() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   echo "i print printer4"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+
+@false
+@calling
+printer5() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   echo My return value is $?, because of @false, but i could be a validator too!
+   echo "i print printer5"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+@false
+@false
+printer6() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   echo "My return value is $?, because of 2 times @false (sum)."
+   echo "i print printer6"
+   echo "---------------------------"
+   echo
+   return 0
+}
+
+
+########
+# MAIN #
+########
+printer1
+printer2
+printer3 Arg1 Arg2
+printer4
+printer5
+printer6
