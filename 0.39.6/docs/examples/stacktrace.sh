7a5d99cbea4cde26e52599aed758f6116947eb96
andreasfrank <andreasfrank@d2084226-57a8-46eb-bcc5-3bd0c7bada0d>
2012-06-22 18:00:08 +0000

    start 0.39.6
    git-svn-id: http://svn.code.sf.net/p/oobash/code@509 d2084226-57a8-46eb-bcc5-3bd0c7bada0d

---
+++ 0.39.6/docs/examples/stacktrace.sh
@@ -0,0 +1,34 @@
+source ../../oobash.source 1>/dev/null 2>&1
+if (( $? != 0 )); then
+   echo "Sorry, but could not find oobash"
+   exit 0
+fi
+
+# overwrites setting in ../../oobash*.source (but there default is 'true', too)
+__STACKTRACE__=true
+
+f3() {
+   declare -i returnValue=0
+   System.out.println "Calling Math.adder 3 5 7 z and 'z' will cause an exception:"
+   System.out.println
+   Math.adder 3 5 7 z
+   returnValue=$?
+   System.out.println
+   return $returnValue
+}
+
+f2() {
+   declare -i returnValue=0
+   f3
+   returnValue=$?
+   return $returnValue
+}
+
+f1() {
+   declare -i returnValue=0
+   f2
+   returnValue=$?
+   return $returnValue
+}
+
+f1
