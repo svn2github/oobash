e276b1a2fda94ec3e3c800b81f774ebd30bc6253
andreasfrank <andreasfrank@d2084226-57a8-46eb-bcc5-3bd0c7bada0d>
2012-08-25 17:34:44 +0000

    renamed dir to oobash
    git-svn-id: http://svn.code.sf.net/p/oobash/code@606 d2084226-57a8-46eb-bcc5-3bd0c7bada0d

---
+++ oobash/docs/examples/decorator.sh
@@ -0,0 +1,97 @@
+source ../../oobash.source 1>/dev/null 2>&1
+if (( $? != 0 )); then
+   echo "Sorry, but could not find oobash"
+   exit 0
+fi
+
+# START -- My own addon decorator
+# Do not use framework commands (e.g. System.out.println) in a decorator !
+@false() {
+   if (( $# != 0 )); then
+      echo  "I am false and will return 1" >&2
+      return 1 
+   fi
+   return 0
+}
+# END -- My own addon decorator
+
+@runtime
+@calling
+@timestamp
+printer1() {
+   __decoratorCheck  "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   System.out.println "i print printer1"
+   System.out.println "---------------------------"
+   System.out.println
+   return 0
+}
+
+@runtime
+@calling
+printer2() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   System.out.println "i print printer2"
+   System.out.println "---------------------------"
+   System.out.println
+   return 0
+}
+
+@runtime
+@deprecated
+@calling
+@timestamp
+printer3() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   System.out.println "i print printer3"
+   System.out.println "---------------------------"
+   System.out.println
+   return 0
+}
+
+printer4() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   System.out.println "i print printer4, i am lonely...no decorator at all!"
+   System.out.println "---------------------------"
+   System.out.println
+   return 0
+}
+
+
+@runtime
+@false
+@calling
+printer5() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   System.out.println "My return value is $?, because of @false, but i could be a validator too!"
+   System.out.println "i print printer5"
+   System.out.println "---------------------------"
+   System.out.println
+   return 0
+}
+
+@runtime
+@false
+@false
+printer6() {
+   __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
+   System.out.println "My return value is $?, because of 2 times @false (sum)."
+   System.out.println "i print printer6"
+   System.out.println "---------------------------"
+   System.out.println
+   return 0
+}
+
+
+########
+# MAIN #
+########
+printer1
+printer2
+cd /
+printer3 Arg1 Arg2
+printer4
+cd "${__OODIR__}"
+printer5
+printer6
+
+System.exit 0
