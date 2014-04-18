fe6b83b56be4726b4a3c92aea3223ea8f1f6ac7b
andreasfrank <andreasfrank@d2084226-57a8-46eb-bcc5-3bd0c7bada0d>
2014-04-18 23:19:22 +0000

    mini changes in decorator.sh
    git-svn-id: http://svn.code.sf.net/p/oobash/code@944 d2084226-57a8-46eb-bcc5-3bd0c7bada0d

--- oobash/docs/examples/decorator.sh
+++ oobash/docs/examples/decorator.sh
@@ -62,7 +62,7 @@ printer4() {
 @calling
 printer5() {
    __decoratorCheck "${FUNCNAME[0]}" "${BASH_SOURCE[0]}" "$@"
-   System.out.println "I see return value is $?, because of @false, but @false could be a validator!"
+   System.out.println "I see return value is $?, because of @false. @false could be a validator!"
    System.out.println "i print printer5"
    System.out.println "---------------------------"
    System.out.println
